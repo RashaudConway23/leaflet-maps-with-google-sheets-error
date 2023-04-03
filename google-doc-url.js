806fb1a4356cbfacd6c91be554a8efbcd1aff845
RashaudConway23 <128190362+RashaudConway23@users.noreply.github.com>
2023-04-03 16:08:03 -0400

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,5 +1,5 @@
 // paste in your published Google Sheets URL from the browser address bar
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1p7P-qlHG4ELpk0NZXiHhK3B7QIAYjCdkPSKLScEWWf8/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1p7P-qlHG4ELpk0NZXiHhK3B7QIAYjCdkPSKLScEWWf8/edit?usp=sharing';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
 var googleApiKey = 'AIzaSyBh9nKnVZm2RPeZa0ywCOxPAgJJfK87WhY';
